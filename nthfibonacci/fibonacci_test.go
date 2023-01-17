dc668dc3e0d402fae96c62fbb0ed08f71a750efc
amitkumar1911 <amit.khan@zopsmart.com>
2023-01-17 21:39:01 +0530

    Performed testing using table driven approach

--- nthfibonacci/fibonacci_test.go
+++ nthfibonacci/fibonacci_test.go
@@ -5,6 +5,34 @@ import (
 	"testing"
 )
 
+func tableDrivenTest(t *testing.T) {
+
+	s := Slc{}
+
+	errorTestCases := []struct {
+		name     string
+		input    int
+		expected []int
+	}{{name: "nequalszero", input: 0, expected: []int{}},
+		{name: "nequalstwo", input: 2, expected: []int{0, 1}},
+		{name: "nequals1", input: 1, expected: []int{0}},
+		{name: "nequalsnegative", input: -2, expected: []int{}},
+		{name: "nequals5", input: 5, expected: []int{0, 1, 1, 2, 3}},
+		{name: "nequals8", input: 8, expected: []int{0, 1, 1, 2, 3, 5, 8, 13}},
+		{name: "nequals4", input: 4, expected: []int{0, 1, 1, 2}}}
+
+	for _, value := range errorTestCases {
+
+		got := s.Fibonacci(value.input)
+
+		want := value.expected
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %q, wanted %q", got, want)
+		}
+	}
+
+}
 func TestFibonacciTwo(t *testing.T) {
 
 	s := Slc{}
@@ -23,68 +51,68 @@ func TestFibonacciTwo(t *testing.T) {
 
 }
 
-func TestFibonacciNegative(t *testing.T) {
+// func TestFibonacciNegative(t *testing.T) {
 
-	s := Slc{}
+// 	s := Slc{}
 
-	got := s.Fibonacci(-4)
+// 	got := s.Fibonacci(-4)
 
-	want := []int{}
+// 	want := []int{}
 
-	// fmt.Println(got)
+// 	// fmt.Println(got)
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %q, wanted %q", got, want)
-	}
+// 	if !reflect.DeepEqual(got, want) {
+// 		t.Errorf("got %q, wanted %q", got, want)
+// 	}
 
-}
+// }
 
-func TestFibonacciFour(t *testing.T) {
+// func TestFibonacciFour(t *testing.T) {
 
-	s := Slc{}
+// 	s := Slc{}
 
-	got := s.Fibonacci(4)
+// 	got := s.Fibonacci(4)
 
-	want := []int{0, 1, 1, 2}
+// 	want := []int{0, 1, 1, 2}
 
-	// fmt.Println(got)
+// 	// fmt.Println(got)
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %q, wanted %q", got, want)
-	}
+// 	if !reflect.DeepEqual(got, want) {
+// 		t.Errorf("got %q, wanted %q", got, want)
+// 	}
 
-}
+// }
 
-func TestFibonacciFive(t *testing.T) {
+// func TestFibonacciFive(t *testing.T) {
 
-	s := Slc{}
+// 	s := Slc{}
 
-	got := s.Fibonacci(5)
+// 	got := s.Fibonacci(5)
 
-	want := []int{0, 1, 1, 2, 3}
+// 	want := []int{0, 1, 1, 2, 3}
 
-	// fmt.Println(got)
+// 	// fmt.Println(got)
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %q, wanted %q", got, want)
-	}
+// 	if !reflect.DeepEqual(got, want) {
+// 		t.Errorf("got %q, wanted %q", got, want)
+// 	}
 
-}
+// }
 
-func TestFibonacciThree(t *testing.T) {
+// func TestFibonacciThree(t *testing.T) {
 
-	s := Slc{}
+// 	s := Slc{}
 
-	got := s.Fibonacci(3)
+// 	got := s.Fibonacci(3)
 
-	// got := value(3)
+// 	// got := value(3)
 
-	want := []int{0, 1, 1}
+// 	want := []int{0, 1, 1}
 
-	// fmt.Println(got)
+// 	// fmt.Println(got)
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %q, wanted %q", got, want)
-	}
+// 	if !reflect.DeepEqual(got, want) {
+// 		t.Errorf("got %q, wanted %q", got, want)
+// 	}
 
-}
+// }
