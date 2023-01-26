027f29d0d85944919f5f2d90cb3593f7cb4db763
amitkumar1911 <amit.khan@zopsmart.com>
2023-01-26 21:53:55 +0530

    used sql mock

--- routing/routes_test.go
+++ routing/routes_test.go
@@ -1,6 +1,7 @@
-package main
+package routing
 
 import (
+	"database/sql"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -8,9 +9,41 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
+func TestGetPersonDetails(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error("Failed to connect with database")
+	}
+
+	rs := mock.NewRows([]string{"name", "age", "phn"}).AddRow("Ankit", 21, "918309172")
+	mock.ExpectQuery("SELECT *").WillReturnRows(rs)
+
+	tests := []struct {
+		name     string
+		database *sql.DB
+		want     []Person
+		wantErr  error
+	}{
+		{name: "Success", database: db, want: []Person{{Name: "Ankit", Age: 21, Phone: "918309172"}}, wantErr: nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getPersonDetails(tt.database)
+			if err != tt.wantErr {
+				t.Errorf("getPersonDetails() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getPersonDetails() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 func Test_rootHandler(t *testing.T) {
 
 	tests := []struct {
